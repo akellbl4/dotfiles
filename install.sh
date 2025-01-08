8dbb9be6ae2550b586c47da5bfa0e6d2a8ea85ea
Paul <paul@mineev.me>
2025-01-08 18:19:00 -0500

    update brew installation command

--- install.sh
+++ install.sh
@@ -26,7 +26,7 @@ if [[ "$1" == "--macos" ]]; then
 	else
 		echo "Brew not found."
 		echo "Installing Homebrew..."
-		/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+		/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 		echo "Brew installed"
 	fi
 
