448e62ed8e0262780435c21762fd8e99e77374fa
Paul Mineev <pmineev@blend.com>
2025-01-08 18:51:12 -0500

    add zshrc, run mac setup only on mac without argument

--- README.md
+++ README.md
@@ -1,9 +1,6 @@
 # Dotfiles
-### Install:
-```
+
+## Install
+```sh
 curl -L https://github.com/akellbl4/dotfiles/raw/master/install.sh | bash
-```
-If you want to install MacOS apps use script with parameter:
-```
-curl -L https://github.com/akellbl4/dotfiles/raw/master/install.sh | bash -s -- --macos
 ```
--- install.sh
+++ install.sh
@@ -1,5 +1,11 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 DOTFILES_DIR="$HOME/.dotfiles"
+MAC=$(if [[ $OSTYPE == darwin* ]]; then echo true; else echo false; fi )
+DOTFILES=(
+	.zshrc
+	.editorconfig
+	.gitconfig
+)
 
 if [[ -d $DOTFILES_DIR ]]; then
 	echo "Updating dotfiles..."
@@ -11,37 +17,43 @@ else
 	git clone https://github.com/akellbl4/dotfiles.git $DOTFILES_DIR
 	echo "Dotfiles downloaded"
 
-	if [[ "$1" == "--macos" && "$2" == "--change-defaults" ]]; then
+	if [[ $MAC == true && "$2" == "--change-defaults" ]]; then
  		echo "Applying mac defaults"
 		source macos_custom_settings.sh
 	fi
 fi
 
-if [[ "$1" == "--macos" ]]; then
-	if [[ -n $(brew -v | grep -iE "^homebrew") ]]; then
-		echo "Run brew updates"
-		brew doctor
-		brew update
-		brew upgrade
-	else
-		echo "Installing Homebrew..."
-		/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-		echo "Homebrew installed"
-	fi
-
-	echo "Installing packages from brew"
-	brew bundle
+if [[ $MAC == false ]]; then
+	exit 1;
 fi
 
-declare -a files=(
-	.editorconfig
-	.gitconfig
-)
+echo "Checking for Homebrew..."
+if [[ -n $(brew -v | grep -iE "^homebrew") ]]; then
+	echo "Updating homebrew..."
+	brew doctor  --quiet
+	brew update  --quiet
+	brew upgrade  --quiet
+	echo "Homebrew updated"
+else
+	echo "Installing Homebrew..."
+	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+	eval "$(/opt/homebrew/bin/brew shellenv)"
+	echo "Homebrew installed"
+fi
+
+echo "Installing packages from Homebrew..."
+brew bundle --quiet --file=$DOTFILES_DIR/Brewfile
+
+
 
 echo "Linking dotfiles..."
-for i in ${files[@]}; do
+for file in ${DOTFILES[@]}; do
 	# create symbolic link if not exists
-	[[ -L $HOME/$i ]] || ln -sv $DOTFILES_DIR/$i $HOME/$i
+	if [[ -L $HOME/$file ]]; then 
+		rm -rf $HOME/$file
+	fi
+
+	ln -sv $DOTFILES_DIR/$file $HOME/$file
 done
 echo
 echo "Done."
